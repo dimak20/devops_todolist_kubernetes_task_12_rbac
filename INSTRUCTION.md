40c8218c19cf79c9a869803b32abcc0efdfb4e47
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-10-15 02:38:57 +0300

    Implement solution

---
+++ INSTRUCTION.md
@@ -0,0 +1,16 @@
+### 1. Run script to set up cluster, databases, apps, ingress, rbac:
+```bash
+./bootstrap.sh
+```
+
+### 2. Get the pod name to exec the command:
+```bash
+kubectl get pods -n todoapp
+```
+
+### 3. Validate rbac access to list the secrets
+You do not need to receive 403 status code
+```bash
+kubectl exec -n todoapp --it <pod-name> -- sh
+SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount APISERVER=https://kubernetes.default.svc TOKEN=$(cat ${SERVICEACCOUNT}/token) CACERT=${SERVICEACCOUNT}/ca.crt curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X GET ${APISERVER}/api/v1/namespaces/todoapp/secrets
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
+
+echo "Creating cluster..."
+kind create cluster --config cluster.yml
+kubectl wait --for=condition=Ready nodes --all --timeout=120s
+
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
 kubectl apply -f .infrastructure/mysql/service.yml
 kubectl apply -f .infrastructure/mysql/statefulSet.yml
 
+kubectl -n mysql rollout status statefulset/mysql --timeout=120s
+
+
 kubectl apply -f .infrastructure/app/ns.yml
 kubectl apply -f .infrastructure/app/pv.yml
 kubectl apply -f .infrastructure/app/pvc.yml
+kubectl apply -f security/rbac.yml
 kubectl apply -f .infrastructure/app/secret.yml
 kubectl apply -f .infrastructure/app/configMap.yml
 kubectl apply -f .infrastructure/app/clusterIp.yml
 kubectl apply -f .infrastructure/app/nodeport.yml
 kubectl apply -f .infrastructure/app/hpa.yml
 kubectl apply -f .infrastructure/app/deployment.yml
+kubectl -n todoapp rollout status deployment/todoapp --timeout=120s
 
+echo "Set up ingress..."
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-# kubectl apply -f .infrastructure/ingress/ingress.yml
+kubectl -n ingress-nginx rollout status deployment/ingress-nginx-controller --timeout=120s
+kubectl apply -f .infrastructure/ingress/ingress.yml
